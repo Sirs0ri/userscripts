3239447d52a207df18c7cd6526436266f4e5a5b6
Maximilian Ertl <Sirs0ri@users.noreply.github.com>
2022-12-05 21:33:45 +0100

    feat(mastodon_emotes_hover): apply styling in more (and less) places!
    There's a changelog inside the file, this is v0.2

--- mastodon_emotes_hover.js
+++ mastodon_emotes_hover.js
@@ -1,43 +1,85 @@
 // ==UserScript==
 // @name         CortexImplant CSS Improvements
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      0.2
+// @description  Improve icons' visibility on hover
+// @author       @Sirs0ri
 // @match        https://corteximplant.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=corteximplant.com
+// @supportURL   https://github.com/Sirs0ri/userscripts/issues
 // @grant        GM_addStyle
 // ==/UserScript==
 
-/*
- * Add a hover-preview for emojione emotes.
- * This straight up removes overflow: hidden on the emoji's parents,
- * I'd be surprised if that *doesn't* break something!
+/* 
+ * ==KNOWN ISSUES==
+ *    - This straight up removes overflow: hidden on the emoji's parents,
+ *      I'd be surprised if that *doesn't* break something!
+ *    - Firefox doesn't support :has() yet, unless you manually turn it on via the layout.css.has-selector.enabled flag
  *
- * Known Issues:
- *   - Emojis in a username work in chrome, but not in Firefox
- *   - Emojis in a user's bio are still clipped by some `pverflow: hidden`
- *   - the :has() CSS selector is relatively new and not yet fully supported in FF (as of 2022-12-06)
- *   - doesn't work for "X now follows you" notifications
+ * ==CHANGES==
+ * 0.2: The "all the small things I missed" patch
+ *    - Add support for DMs, Boots, Notifications, Profiles
+ *    - In addition to zooming, make sure hovered icons are in front of other content and fully opaque
+ *    - Since this heavly relies on the relatively new :has() CSS selector, disable the styling when :has() isn't supported
+ *    - Improve documentation
+ *
+ * 0.1: initial release
+ *    - Add highlight-on-hover to emojione emotes in toots
  */
 
 (function() {
     'use strict';
 
-    // Use TamperMonkeys helper to inject CSS:
+    // Use TamperMonkey's helper to inject CSS
+    // This heavily relies on :has(), without it the styling has no effect due to the @supports query.
+    // The :where(...):has(...) statement is used to select a bunch of wrappers that
+    // would otherwise have overflow: hidden, and remove that while an image is hovered.
     GM_addStyle(`
-:is(
-  .status__content,
-  .display-name,
-  .status__display-name
-):has(img.emojione:hover) {
+@supports selector(:has(a, b)) {
+  /* Temporarily disable overflow on elements restraining the images while hovering */
+  :where(
+    /* Toots */
+    .status__content,
+    .display-name,
+    .status__display-name,
+
+    /* DMs */
+    .conversation__content__names,
+    .conversation__content__info,
+    .conversation__content,
+
+    /* Boosts */
+    .status__prepend>span,
+
+    /* follow notifications */
+    .notification__message>span,
+    .account__display-name strong,
+    .account .account__display-name,
+
+    /* Profiles */
+    .account__header__tabs__name h1,
+    .account__header__content,
+
+    /* Profile Info Table */
+    .account__header__fields dd,
+    .account__header__fields dt
+  ):has(img.emojione:hover) {
     overflow: revert;
-}
-:not(.emoji-button)>img.emojione {
-    transition: transform 200ms;
-}
-:not(.emoji-button)>img.emojione:hover {
+  }
+
+  /* Add Transition to make it look nice */
+  :not(.emoji-button)>img.emojione {
+    transition:
+      transform 200ms,
+      opacity 200ms;
+  }
+  /* Finally, scale emojis on hover and make sure they're fully visible */
+  :not(.emoji-button)>img.emojione:hover {
     transform: scale(5);
+    opacity: 1;
+    z-index: 1;
+    position: relative;
+  }
 }
 `)
 })();
