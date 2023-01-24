0092a0f5e1a985bea3cb21fab75381c85e56e8d8
Maximilian Ertl <Sirs0ri@users.noreply.github.com>
2023-01-25 00:52:38 +0100

    Lots of fixes and changes to the "neonification"
    UX changes
    * Larger clickable area of posts, try opening an image post without text!
    * selected posts have a more visible highlight
    
    UI changes:
    * new styling for:
      * compose panel (with animations!)
      * scrollbars
      * various popups (search, profile settings)
      * about page
      * profile directory
    * main headers over feeds now see-through, buttons inside headers are properly styled
    * notifications have a glow based on their type (fav, reply, other)
    * better neon effect on interactive elements
    
    Fixes:
    * Prevent videos being covered by blurred version
    * Better alignment of the replaced CORTEX IMPLANT logo

--- cortex_implant_styling.js
+++ cortex_implant_styling.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CortexImplant CSS Improvements
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      1.0
 // @description  Change the styling for the mastodon instance I'm on
 // @author       @Sirs0ri
 // @match        https://corteximplant.com/*
@@ -28,6 +28,7 @@
     // see https://codepen.io/mattgrosswork/pen/VwprebG
 
     GM_addStyle(`
+
 /* ====================
  * Misc general changes
  * ==================== */
@@ -40,7 +41,7 @@ a.button {
 /* wider margin next to the main content scroller */
 @media screen and (min-width: 1175px) {
     .columns-area__panels__main {
-        margin: 0 clamp(0px, calc(3vw - 36px), 38px);
+        margin: 0 clamp(0px, calc(4vw - 48px), 50px);
     }
 
     .compose-form {
@@ -61,9 +62,56 @@ a.button {
 .columns-area--mobile article:empty {
     display: none;
 }
+
+/* Make clickable area of posts larger */
+
+.status__content--with-action {
+    margin-top: -72px;
+    padding-top: 72px;
+}
+.status__prepend + .status .status__content--with-action {
+    margin-top: -110px;
+    padding-top: 110px;
+}
+.status__content--with-action:nth-last-child(3) {
+    /* Followed by a media gallery */
+    margin-bottom: -16px;
+    padding-bottom: 16px;
+}
+.status__prepend a,
+.status__info a {
+    z-index: 1;
+    position: relative;
+}
 `)
 
     GM_addStyle(`
+/* Colors
+
+posts
+search__input
+	hsl(227deg 16% 19%)
+
+active post
+.scrollable.about:before
+header
+	hsl(227deg 16% 23%)
+
+posts border
+lists border
+	hsl(227deg 16% 27%)
+
+header border
+.column-header__collapsible
+.search__input border
+	hsl(227deg 16% 31%)
+
+.scrollable.about:before border
+header:hover border
+	hsl(227deg 16% 51%)
+*/
+
+
 /* ====================
  *   Neon-ify the page
  * ====================*/
@@ -88,13 +136,15 @@ a.button {
             0 0 151px hsla(var(--neon-color) / 1);
         --neon-box-shadow-small:
             /* White glow */
-            0 0 .2rem hsla(var(--white-hsl) / 0.1),
-            0 0 .2rem hsla(var(--white-hsl) / 0.1),
+            0 0 6px -2px hsla(var(--white-hsl) / 0.4),
+            0 0 10px hsla(var(--white-hsl) / 0.2),
+            0 0 15px hsla(var(--white-hsl) / 0.1),
             /* Colored glow */
-            0 0 2rem hsla(var(--neon-color) / 0.05),
-            0 0 0.8rem hsla(var(--neon-color) / 0.05),
-            0 0 2.8rem hsla(var(--neon-color) / 0.05),
-            inset 0 0 1.3rem hsla(var(--neon-color) / 0.05);
+            0 0 10px hsla(var(--neon-color) / 0.07),
+            0 0 25px hsla(var(--neon-color) / 0.05),
+            0 0 47px hsla(var(--neon-color) / 0.12),
+            /* White inside glow */
+            inset 0 0 5px hsla(var(--white-hsl) / 0.3)
     }
 
     /* ===== Buttons get a glow-on-hover ===== */
@@ -105,7 +155,7 @@ a.button {
         transition: box-shadow 200ms, outline-color 200ms;
     }
     .button:hover {
-        outline-color: hsl(240deg 100% 73%);
+        outline-color: hsl(240deg 100% 83%);
         box-shadow: var(--neon-box-shadow-small);
     }
 
@@ -119,15 +169,121 @@ a.button {
     }
 
     /* Glowy horizontal lines */
-    .compose-panel hr,
-    .navigation-panel hr {
+    :is(#fake, .compose-panel, .navigation-panel) hr {
         box-shadow: var(--neon-box-shadow);
-        border-top-color: white;
-        border-top-width: 1px;
+        border-top: 1px solid hsl(227deg 16% 81%);
         position: relative;
         z-index: -1;
         margin: 20px 0;
+        height: 0;
     }
+
+    :is(#fake, .compose-panel) .spacer {
+        margin: 30px 0 !important;
+    }
+
+    /* Profile Menu */
+    .dropdown-menu.bottom {
+        border-radius: 8px;
+        padding: 8px 0;
+    }
+
+    /* Search popup */
+    .search-popout {
+        border-radius: 8px;
+    }
+
+    /* ===== Compose form ===== */
+
+    /* I'm abusing a property of :is() to avoid an !important here.
+     * :is()'s specificity is the highest spec. of all selectors in it,
+     * even if those selectors are invalid. Using :is() with a valid
+     * .class selector and an invalid #id selector bumps up the spec.
+     * for the whole :is() selector from .class to #id which allows
+     * it to override some default styles.
+     * A declaration with !important would still take precedence.
+     * Docs: https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity
+     */
+
+     /* I'm not *quite* happy with these colors. Waiting for more inspiration */
+
+    :is(#fakeId, .compose-form__autosuggest-wrapper) {
+        border-radius: 8px;
+        border: 1px solid hsl(227deg 16% 51%);
+        padding: 5px 0;
+        /* Make sure this is above the autosuggest window at z 99 */
+        z-index: 100;
+        transition: box-shadow 200ms;
+    }
+    :is(#fakeId, .compose-form__autosuggest-wrapper):focus-within {
+        box-shadow: var(--neon-box-shadow-small);
+    }
+    :is(#fakeId, .compose-form__autosuggest-wrapper) .autosuggest-textarea__textarea,
+    :is(#fakeId, .compose-form__autosuggest-wrapper) .compose-form__modifiers {
+        background: none;
+    }
+
+    .compose-form .emoji-picker-dropdown {
+        top: 6px;
+        right: 2px;
+    }
+
+    /* Suggestions box */
+    .compose-form .autosuggest-textarea__suggestions.autosuggest-textarea__suggestions--visible {
+        border-radius: 8px;
+        margin-top: -20px;
+        padding-top: 26px; /* This has a default padding of 6px */
+    }
+
+    .compose-form .autosuggest-textarea__suggestions.autosuggest-textarea__suggestions--visible,
+    :is(#fakeId, .compose-form__buttons-wrapper) {
+        border: 1px solid hsl(227deg 16% 51%);
+    }
+
+    :is(#fakeId, .compose-form__buttons-wrapper) {
+        border-radius: 8px;
+        margin-top: -20px;
+        padding-top: 30px;
+        background: hsl(227deg 16% 81%);
+    }
+    .compose-form__buttons button {
+        min-width: 27px;
+        border-radius: 8px;
+    }
+
+    /* This is actually the language selector */
+    .privacy-dropdown.active .privacy-dropdown__value {
+        border-radius: 8px 8px 0 0;
+    }
+    .language-dropdown__dropdown {
+        border-radius: 8px;
+    }
+
+    .reply-indicator {
+        border-radius: 8px;
+        margin-bottom: 20px;
+    }
+
+
+    .compose-form .spoiler-input__input {
+        border-radius: 8px;
+    }
+    .compose-form .spoiler-input {
+        transition: height .4s ease, opacity .4s ease, margin-bottom 0.2s ease 0.1s;
+
+        background: white;
+        border-radius: 8px;
+        border: 1px solid hsl(227deg 16% 51%);
+        box-sizing: border-box;
+        padding-top: 5px;
+        margin-bottom: -7px;
+    }
+    .compose-form .spoiler-input.spoiler-input--visible {
+        margin-bottom: -20px;
+
+        height: 69px; /* nice */
+    }
+
 
     /* Adjust Horizontal align of the first <hr> with the first post in a feed */
     .navigation-panel__logo hr {
@@ -176,18 +332,51 @@ a.button {
     .tabs-bar__wrapper {
         background: none;
     }
-    .column-header__wrapper,
+
     .search__input {
         border-radius: 8px;
         outline: 1px solid hsl(227deg 16% 31%);
         outline-offset: -1px;
+        background: hsl(224deg 16% 19%);
     }
-    .column-header__wrapper {
-        background: #313543;
+
+    #tabs-bar__portal:not(:empty) {
+        border-radius: 8px
+    }
+
+    #tabs-bar__portal:not(:empty):after {
+        content: "";
+        position: absolute;
+        inset: 0px;
+        border-radius: inherit;
+        border: 1px solid hsl(227deg 16% 31%);
+        z-index: 1;
+        pointer-events: none;
+    }
+
+    #tabs-bar__portal:not(:empty) {
+        backdrop-filter: blur(3px);
+        background: hsla(227deg 16% 23% / 0.8);
         transition: box-shadow 200ms;
     }
-    .column-header__wrapper:hover {
+    #tabs-bar__portal:not(:empty):hover {
         box-shadow: var(--neon-box-shadow-small);
+    }
+    #tabs-bar__portal:not(:empty):hover:after {
+        border-color: hsl(227deg 16% 51%);
+    }
+
+    .column-back-button {
+        background: none;
+        border-radius: inherit;
+    }
+    .column-back-button:hover,
+    .column-header__back-button:hover {
+        text-decoration: none;
+    }
+
+    .column-header__wrapper.active {
+        box-shadow: none;
     }
 
     .column-header,
@@ -196,6 +385,11 @@ a.button {
     .column-header__back-button {
         background: none;
     }
+
+    .column-header__button.active {
+        border-radius: 8px 8px 0 0;
+    }
+
     .column-header__collapsible {
         border-radius: 0 0 8px 8px;
         background: hsl(227deg 16% 31%);
@@ -214,6 +408,10 @@ a.button {
         contain: style;
         margin-top: 20px;
     }
+    .columns-area--mobile {
+        padding: 0 10px 10px;
+        box-sizing: border-box;
+    }
 
     /* ===== Posts styling ===== /*
 
@@ -225,12 +423,14 @@ a.button {
     .columns-area--mobile .scrollable>div>div[tabindex="-1"]
     {
         margin-bottom: 20px;
-        border: 1px solid hsl(224deg 16% 27%);
-        background: hsl(224deg 16% 19%);
+        border: 1px solid hsl(227deg 16% 27%);
+        background: hsl(227deg 16% 19%);
         border-radius: 8px;
     }
     /* give the selected post in single-post-view a lighter background */
     .columns-area--mobile .scrollable>div[tabindex="-1"] {
+        box-shadow: var(--neon-box-shadow-small);
+        border-color: hsl(227deg 16% 51%);
         background: hsl(227deg 16% 23%);
     }
 
@@ -253,6 +453,11 @@ a.button {
     .columns-area--mobile article>div>.status__wrapper.unread:before {
         border-radius: inherit;
     }
+
+    .status__wrapper--filtered {
+        border: none;
+    }
+
 
     /* Lists are still TODO because I don't use them */
     .column-subheading {
@@ -282,6 +487,7 @@ a.button {
     }
 
     /* Notifications */
+
     .notification__filter-bar,
     .account__section-headline {
         margin-top: -20px;
@@ -293,6 +499,39 @@ a.button {
     .notification__filter-bar button {
         /* background: none;*/
         border-radius: inherit;
+    }
+
+    /* Mentions use .status__wrapper directly, all other notifications are wrapped in a .notification div */
+    .status__wrapper.unread:after,
+    .notification.unread:after {
+        content: "";
+        position: absolute;
+        top: 4px;
+        bottom: 4px;
+        left: 2px;
+        width: 1px;
+        background: white;
+        box-shadow: var(--neon-box-shadow);
+        z-index: -1;
+    }
+
+    .notification-favourite {
+        background: rgba(202 143 4 / 0.05);
+    }
+    :is(#fake, .notification-favourite):before {
+        border-color: rgba(202 143 4 / 1);
+    }
+
+    .notification-follow,
+    .notification-reblog {
+        background: rgba(140 141 255 / 0.05);
+    }
+    :is(#fake, .notification-follow, .notification-reblog):before {
+        border-color: rgba(140 141 255 / 1);
+    }
+
+    .status__wrapper.unread:before {
+        border-color: hsl(239deg 100% 90%);
     }
 
     /* ===== Profile ===== */
@@ -343,6 +582,75 @@ a.button {
         border-radius: 8px;
     }
 
+    /* ===== About page ===== */
+
+    .scrollable.about {
+        margin-top: 0;
+        padding: 20px 0px;
+    }
+
+    .scrollable.about:before {
+        content: "";
+        display: block;
+        position: relative;
+        top: -20px;
+        left: -10px;
+        height: 48px;
+        width: calc(100% + 20px);
+        background: hsl(227deg 16% 23%);
+        box-sizing: border-box;
+        border-radius: 8px;
+        border-left: 1px solid;
+        border-right: 1px solid;
+        border-color: hsl(227deg 16% 51%);
+        clip-path: polygon(0 0, 5% 0%, 3% 85%, 5% 85%, 7% 0%, 9% 0%, 7% 85%, 9% 85%, 11% 0%, 100% 0, 100% 100%, 95% 100%, 97% 15%, 95% 15%, 93% 100%, 91% 100%, 93% 15%, 91% 15%, 89% 100%, 13% 100%, 14% 57.5%, 16% 57.5%, 15% 100%, 15% 85%, 88% 85%, 87% 100%, 89% 15%, 13% 15%, 11% 100%, 0 100%);
+    }
+
+    .about__header__hero.loaded.image {
+        overflow: revert;
+    }
+    .about__header__hero.loaded .image__preview {
+        display: revert;
+        z-index: -1;
+        filter: blur(5px);
+        border-radius: inherit;
+    }
+    .about__header__hero.loaded img {
+        border-radius: inherit;
+    }
+    .about__meta,
+    .about__section,
+    .about__domain-blocks {
+        border-radius: 8px;
+    }
+
+    .about__section__title,
+    .about__section__body {
+        border-radius: inherit !important;
+    }
+    .about__section__body {
+        margin-top: -20px;
+        padding-top: 40px;
+    }
+
+
+    /* ===== Profile directory ===== */
+     .account-card {
+         margin: 20px 0 !important;
+         border-radius: 8px;
+         border: 1px solid hsl(224deg 16% 35%);
+     }
+     .account-card__permalink,
+     .account-card__header,
+     .account-card__header img{
+         border-radius: inherit;
+     }
+
+     .account-card__header {
+         padding: 0;
+     }
+
+
     /* ====================
      *    Glow on Media
      * ==================== */
@@ -380,7 +688,7 @@ a.button {
         filter: blur(4px);
     }
     /* Undo hiding the blurhash canvas unless it's for part of a video in full screen */
-    :not(.fullscreen)>.media-gallery__preview--hidden {
+    :not(.fullscreen, .detailed)>.media-gallery__preview--hidden {
         display: revert;
         filter: blur(4px);
     }
@@ -400,11 +708,61 @@ a.button {
         mix-blend-mode: plus-lighter;
         pointer-events: none;
     }
+
+    /* ====================
+     *     Scrollbars
+     * ==================== */
+
+    html {
+        scrollbar-color: hsl(227deg 16% 23%) rgba(0,0,0,.1);
+    }
+
+    ::-webkit-scrollbar {
+        width: 12px;
+        height: 12px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        margin: 0 2px;
+        width: 8px;
+        background: hsl(227deg 16% 18%);
+        border: 1px solid hsl(225deg 16% 28%);
+        border-radius: 50px;
+
+        transition: background-color 200ms;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background: hsl(225deg 16% 28%);
+        border-color: hsl(227deg 16% 38%);
+    }
+
+    ::-webkit-scrollbar-thumb:active {
+        background: hsl(227deg 16% 38%);
+        border-color: hsl(227deg 16% 48%);
+    }
+
+    ::-webkit-scrollbar-track {
+        border: 0 #fff;
+        border-radius: 0;
+        background: rgba(0,0,0,.1);
+    }
+
+    ::-webkit-scrollbar-track:active,
+    ::-webkit-scrollbar-track:hover {
+        background: hsl(224deg 16% 19%);
+        background: rgba(0,0,0,.2);
+    }
+
+    ::-webkit-scrollbar-corner {
+        background: transparent:
+    }
 }
-    `)
+`)
 
     GM_addStyle(`
 /* Emoji picker bigger images */
+
 .emoji-mart-category-list {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(42px, 1fr));
@@ -454,7 +812,7 @@ a.button {
 span.relationship-tag {
     border-radius: 8px;
     background: rgba(0 0 0 / 0.6);
-    color: hsla(0,0%,100%,.7);
+    color: hsla(0 0% 100% / .7);
     opacity: 1;
 }
 
@@ -463,7 +821,7 @@ span.relationship-tag {
 .media-gallery__item:has(video[aria-label]):after{
     content: "alt";
     background: rgba(0 0 0 / 0.6);
-    color: hsla(0,0%,100%,.7);
+    color: hsla(0 0% 100% / .7);
     position: absolute;
     top: 4px;
     right: 4px;
@@ -479,20 +837,9 @@ span.relationship-tag {
 .audio-player:has(canvas.audio-player__canvas[aria-label]) .video-player__buttons.right:before,
 .video-player:has(video[aria-label]) .video-player__buttons.right:before{
     content: "alt";
-    color: hsla(0,0%,100%,.7);
+    color: hsla(0 0% 100% / .7);
     padding: 0 5px;
     pointer-events: none;
-}
-`)
-
-    GM_addStyle(`
-    /* ===== Recolor different notifocations ===== */
-.notification-favourite {
-    background: rgba(202 143 4 / 0.05);
-}
-.notification-follow,
-.notification-reblog{
-    background: rgba(140 141 255 / 0.05);
 }
 `)
 
@@ -512,10 +859,12 @@ a.ui__header__logo:before {
     display: block;
     background-size: contain;
     background-repeat: no-repeat;
+    background-position: center;
+    margin: -10px;
 }
 
 a.column-link.column-link--logo:before {
-    height: 34px;
+    height: 48px;
 }
 
 a.ui__header__logo:before {
@@ -531,16 +880,11 @@ a.ui__header__logo:before {
 /* Glitch Effect on notifications */
 
 .column-link--transparent:has(i+i) {
-  /* font-size: clamp(40px, 10vw, 100px); */
   position: relative;
-  /* color: #fff; */
   z-index: 2;
   --padding: 15px;
   --start: 25px;
   padding: var(--padding);
-
-  /* Bright things in dark environments usually cast that light, giving off a glow */
-  /* filter: drop-shadow(0 1px 3px); */
 
   text-decoration: none;
 }
