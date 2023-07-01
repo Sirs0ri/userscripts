d0731868f50ea01e7283b9a9de48cb2573bd4b92
Maximilian Ertl <Sirs0ri@users.noreply.github.com>
2023-07-01 18:54:40 +0200

    Create themed-toot-button-states.js

---
+++ themed-toot-button-states.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Toot button states
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Add better distinguishable "disabled" and "hover" states to a toot button with a colorful background
+// @author       Sirs0ri
+// @match        https://corteximplant.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=corteximplant.com
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle(`
+
+:where(.compose-form__publish-button-wrapper button, .ui__header__links .button):before {
+    content: "";
+    position: absolute;
+    inset: -1px;
+    transition: opacity 200ms;
+    opacity: 0;
+}
+:where(.compose-form__publish-button-wrapper button, .ui__header__links .button)[disabled]:before {
+    backdrop-filter: saturate(0.65) brightness(1.4) contrast(0.75) blur(1px);
+    opacity: 1;
+}
+:where(.compose-form__publish-button-wrapper button, .ui__header__links .button)[disabled]:hover:before {
+    opacity: 0.5;
+}
+
+`)
+})();
