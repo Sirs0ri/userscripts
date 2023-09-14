2a42b997907be21fa1632119c0d8ad1ae3c04824
Maximilian Ertl <ertl.maxi@gmail.com>
2023-09-14 20:58:28 +0200

    build: bump version to b.16

--- cortex_implant_styling.js
+++ cortex_implant_styling.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CortexImplant CSS Improvements
 // @namespace    http://tampermonkey.net/
-// @version      1.5.3-b.15
+// @version      1.5.3-b.16
 // @description  Change the styling for the mastodon instance I'm on
 // @author       @Sirs0ri
 // @match        https://corteximplant.com/*
