11c662f4aa06d2b9a153ea025b76aebd0b724daa
Maximilian Ertl <Sirs0ri@users.noreply.github.com>
2023-11-29 00:11:43 +0100

    Update remove-subscription-banners.user.js

--- remove-subscription-banners.user.js
+++ remove-subscription-banners.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Block Pay2Win Banners
 // @namespace    https://github.com/Sirs0ri/userscripts
-// @version      1.0
+// @version      1.1.0
 // @description  Removes nagging "Subscribe or let us sell your data" banners
 // @author       https://corteximplant.com/@Sirs0ri
 // @updateURL    https://raw.githubusercontent.com/Sirs0ri/userscripts/main/remove-subscription-banners.js
@@ -14,12 +14,12 @@
 (function() {
     'use strict';
 
-    if (!window._sp_?.destroyMessaging) return
+    if (!window._sp_?.destroyMessages) return
 
     const listener = () => {
         window.requestAnimationFrame(() => {
             console.warn("Hooray! Pay2win banner removed")
-            window._sp_.destroyMessaging()
+            window._sp_.destroyMessages()
         })
     }
     window._sp_.addEventListener("onMessageReady", listener)
