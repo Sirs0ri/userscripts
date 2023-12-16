b737201259dcc830809ab0fceed32a28c5d1d089
Maximilian Ertl <ertl.maxi@gmail.com>
2023-12-16 20:54:13 +0100

    feat: make script auto-installable and auto-updateable

--- cortex_implant_styling.user.js
+++ cortex_implant_styling.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         CortexImplant CSS Improvements
 // @namespace    http://tampermonkey.net/
-// @version      1.6.0-b.26
+// @version      1.6.0-b.27
 // @description  Change the styling for the mastodon instance I'm on
 // @author       @Sirs0ri
+// @updateURL    https://raw.githubusercontent.com/Sirs0ri/userscripts/develop/cortex_implant_styling.user.js
+// @downloadURL  https://raw.githubusercontent.com/Sirs0ri/userscripts/develop/cortex_implant_styling.user.js
+// @supportURL   https://github.com/Sirs0ri/userscripts/issues
 // @match        https://corteximplant.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=corteximplant.com
-// @supportURL   https://github.com/Sirs0ri/userscripts/issues
 // @grant        GM_addStyle
 // @grant        GM_getResourceURL
 //
