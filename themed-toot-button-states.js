bbafe98f33f03039584e6129f4604501c7ae47b9
Maximilian Ertl <Sirs0ri@users.noreply.github.com>
2024-01-26 19:23:31 +0100

    fix: blurred text with themed toot button

--- themed-toot-button-states.js
+++ themed-toot-button-states.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name         Toot button states
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1.0
 // @description  Add better distinguishable "disabled" and "hover" states to a toot button with a colorful background
+// @updateURL    https://raw.githubusercontent.com/Sirs0ri/userscripts/main/themed-toot-button-states.js
+// @downloadURL  https://raw.githubusercontent.com/Sirs0ri/userscripts/main/themed-toot-button-states.js
 // @author       Sirs0ri
 // @match        https://corteximplant.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=corteximplant.com
@@ -13,7 +15,9 @@
     'use strict';
 
     GM_addStyle(`
-
+:where(.compose-form__publish-button-wrapper button, .ui__header__links .button):after {
+    z-index: 1;
+}
 :where(.compose-form__publish-button-wrapper button, .ui__header__links .button):before {
     content: "";
     position: absolute;
@@ -28,6 +32,5 @@
 :where(.compose-form__publish-button-wrapper button, .ui__header__links .button)[disabled]:hover:before {
     opacity: 0.5;
 }
-
 `)
 })();
